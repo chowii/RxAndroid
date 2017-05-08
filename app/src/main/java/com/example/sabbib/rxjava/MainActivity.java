2fad7a7db25f450d41073daecf856f190e3eea04
Sabbib Chowdhury <sabbib@mentallyfriendly.com>
2017-05-08 12:43:41 +1000

    Reads and saves a single image from url using Rx

--- app/src/main/java/com/example/sabbib/rxjava/MainActivity.java
+++ app/src/main/java/com/example/sabbib/rxjava/MainActivity.java
@@ -4,7 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.sabbib.rxjava.Gist.GistObservable;
+import com.example.sabbib.rxjava.Gist.GistObservableActivity;
+import com.example.sabbib.rxjava.image.ImageObservableActivity;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -12,9 +13,11 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.gistButton)
-    public void onClick(){
-        startActivity(new Intent(this, GistObservable.class));
-    }
+    public void gistActivity(){ startActivity(new Intent(this, GistObservableActivity.class)); }
+
+    @OnClick(R.id.imageButton)
+    public void imageActivity(){ startActivity(new Intent(this, ImageObservableActivity.class)); }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
